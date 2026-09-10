66e65461d9df9d778ee194c59dd84f40bf3e2c05
yyCharlotte <2295608472@qq.com>
2026-09-10 23:14:49 +0800

    洛谷P1160队列安排，自己重写的链表代码，使用ai辅助完成

--- lgP1160.cpp
+++ lgP1160.cpp
@@ -1,57 +1,112 @@
+// //
+// // Created by y2295 on 2026/9/9.
+// //# P1160 队列安排
 //
-// Created by y2295 on 2026/9/9.
-//# P1160 队列安排
+// #include<iostream>
+// /*#include<cstdio>
+// #include<cstdlib>
+// #include<cstring>*/
+// using namespace std;
+// const int mx=1e5+10;
+// int n,m;
+// struct T{
+//     int l,r;        //每个同学的“左右手”
+//     int d;          //表示同学是否输出
+// }t[mx]={0};
+// void add(int i,int k,int f)       //新增同学
+// {
+//     if(f==1)         //左
+//     {
+//         t[k].r=t[i].r;
+//         t[k].l=i;
+//         t[i].r=k;
+//         t[t[k].r].l=k;
+//     }
+//     else             //右
+//     {
+//         t[k].r=i;
+//         t[k].l=t[i].l;
+//         t[i].l=k;
+//         t[t[k].l].r=k;
+//     }
+// }
+// int main()
+// {
+//     int x,k,f;
+//     cin>>n;
+//     t[0].r=0,t[0].l=0;
+//     add(0,1,1);
+//     for (int i=2;i<=n;i++)
+//     {
+//         cin>>x>>f;
+//         add(x,i,f);
+//     }
+//     cin>>m;
+//     while(m--)
+//     {
+//         cin>>x;
+//         t[x].d=1;         //将该同学标记为不输出
+//     }
+//     for (int i=t[0].r;i;i=t[i].r)
+//     {
+//         if (t[i].d==0)    //输出未标记的
+//             cout<<i<<" ";
+//     }
+//     return 0;
+// }
+//
 
+//9.10复习
 #include<iostream>
-/*#include<cstdio>
-#include<cstdlib>
-#include<cstring>*/
 using namespace std;
-const int mx=1e5+10;
-int n,m;
-struct T{
-    int l,r;        //每个同学的“左右手”
-    int d;          //表示同学是否输出
-}t[mx]={0};
-void add(int i,int k,int f)       //新增同学
-{
-    if(f==1)         //左
-    {
+const int mx = 100010;
+struct T {
+    int l,r;
+    int d;
+}t[mx];
+
+void add(int i,int k,int f) {
+    if (f) {
         t[k].r=t[i].r;
         t[k].l=i;
         t[i].r=k;
         t[t[k].r].l=k;
     }
-    else             //右
-    {
-        t[k].r=i;
+    else {
         t[k].l=t[i].l;
+        t[k].r=i;
         t[i].l=k;
         t[t[k].l].r=k;
     }
 }
-int main()
-{
-    int x,k,f;
-    cin>>n;
-    t[0].r=0,t[0].l=0;
+int main() {
+    int n;
+    cin >> n;
+    t[0].r = 0;
+    t[0].l = 0;
     add(0,1,1);
-    for (int i=2;i<=n;i++)
-    {
+
+    for (int i=2;i<=n;i++) {
+        int x,f;
         cin>>x>>f;
         add(x,i,f);
     }
+    int m;
     cin>>m;
-    while(m--)
-    {
-        cin>>x;
-        t[x].d=1;         //将该同学标记为不输出
+    while (m--) {
+        int x;
+        cin >> x;
+        t[x].d=1;
     }
-    for (int i=t[0].r;i;i=t[i].r)
-    {
-        if (t[i].d==0)    //输出未标记的
+
+    for (int i=t[0].r;i!=0;i=t[i].r) {
+
+        if (t[i].d==0) {
             cout<<i<<" ";
+        }
     }
+
+
+
     return 0;
 }
-
